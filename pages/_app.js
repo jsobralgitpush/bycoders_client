96933a0516e053865588f886a6e5fff22afbe4e1
jose mancebo <jose.sobral@poli.ufrj.br>
2022-06-02 08:40:16 -0300

    Fix _app file to handle <Head> and <main> tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import Head from 'next/head'
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Bycoders Challenge</title>
+      </Head>
+
+      <main>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </main>
+    </>
+  )
 }
 
 export default MyApp
