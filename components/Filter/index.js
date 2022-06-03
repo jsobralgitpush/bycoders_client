449ad8487807080767685957b700227877d070b6
jose mancebo <jose.sobral@poli.ufrj.br>
2022-06-03 20:29:41 -0300

    Refactor Filter Component to handle transactions and store owner selection

--- components/Filter/index.js
+++ components/Filter/index.js
@@ -1,8 +1,25 @@
-export default function Filter() {
+import {
+    FormControl,
+    FormLabel,
+    Box,
+    FormHelperText,
+    Select
+  } from '@chakra-ui/react'
+import getTransactions from '../../services/getTransactions'
 
+export default function Filter({optionsAvailable, setOptionSelected, setTransactions, setBalance}) {
     return (
         <>
-            <h1>Hello</h1>
+            <FormControl>
+                <FormLabel htmlFor='email'>Nome da Loja</FormLabel>
+                <Select onChange={(e) => {
+                    setOptionSelected(e.target.value)
+                    getTransactions(e.target.value, setTransactions, setBalance)
+                }} placeholder='Todos'>
+                    {optionsAvailable.map((option, i) => <option value={option} key={i}>{option}</option>)}
+                </Select>
+                <FormHelperText>Após selecionar uma loja, o saldo ficará disponível</FormHelperText>
+            </FormControl>
         </>
     )
 }
