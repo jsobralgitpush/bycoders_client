cfdbbcc15013193c1d86a380d30d0ce23b966b06
jose mancebo <jose.sobral@poli.ufrj.br>
2022-06-03 20:29:13 -0300

    Refactor Input component to handle postTransactions

--- components/Input/index.js
+++ components/Input/index.js
@@ -1,9 +1,12 @@
-export default function FileUploader() {
-    const handleFileInput = () => {}
+import postTransactions from "../../services/postTransactions"
 
+export default function FileUploader({optionSelected, setTransactions, setBalance}) {
     return (
         <div className="file-uploader">
-            <input type="file" onChange={handleFileInput} />
+            <input type="file" onChange={(e) => {
+                postTransactions(e.target.files[0], setTransactions, setBalance, optionSelected)
+                e.target.value = ""
+            }} />
         </div>
     )
 }
