1761668ea8f755e2b71c054b30c49250c5255dae
jose mancebo <jose.sobral@poli.ufrj.br>
2022-06-03 20:30:39 -0300

    Refactor Home component, adding FileUploader, Filter and Transactions components

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,40 @@
 import FileUploader from "../components/Input";
 import Filter from "../components/Filter";
 import ListTransactions from "../components/ListTransactions";
-import { Container, Box, SimpleGrid } from '@chakra-ui/react'
+import { Box, SimpleGrid } from '@chakra-ui/react'
+import { useState } from 'react'
+
+export default function Home({initialScope}) {
+  const [transactions, setTransactions] = useState(initialScope);
+  const [optionSelected, setOptionSelected] = useState(null);
+  const [balance, setBalance] = useState(0);
+
+  const options = [...new Set(initialScope.map(transaction => transaction.store_name))]
 
 
-
-export default function Home() {
   return (
     <>
-      <Container>
         <SimpleGrid colums={1} spacing={5}>
           <Box>
-            <FileUploader></FileUploader>
+            <FileUploader optionSelected={optionSelected} setTransactions={setTransactions} setBalance={setBalance}></FileUploader>
           </Box>
           <Box>
-            <Filter></Filter>
+            <Filter setBalance={setBalance} setTransactions={setTransactions} setOptionSelected={setOptionSelected} optionsAvailable={options}></Filter>
           </Box>
+            {optionSelected ? <Box bg={balance > 0 ? 'green' : 'red'} w='100%' p={4} color='white'>Saldo disponível para {optionSelected}: R$ {balance} </Box> : ''}
           <Box>
-            <ListTransactions></ListTransactions>
+            <ListTransactions scope={transactions}></ListTransactions>
           </Box>
         </SimpleGrid>
-      </Container>
     </>
   )
+}
+
+export async function getStaticProps(context) {
+  const res = await fetch('http://localhost:3000/api/transactions');
+  const initialScope = await res.json()
+
+  return {
+      props: {initialScope}
+  }
 }
